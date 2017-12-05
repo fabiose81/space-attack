6f9b4566e9173aeb5f3227a4bb5a67a3d3d1bbab
eleves <eleves@mac404inf-15.local>
2017-12-05 14:45:16 -0500

    Version :: 1.0 - Coding...

--- space-attack/viewcontrollers/MainViewController.swift
+++ space-attack/viewcontrollers/MainViewController.swift
@@ -15,11 +15,14 @@ class MainViewController: UIViewController {
     @IBOutlet weak var satellite: UIImageView!
     @IBOutlet weak var alien: UIImageView!
     @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var explosion: UIImageView!
+    @IBOutlet weak var comet: UIImageView!
     
     @IBOutlet weak var shot: UIButton!
     
     var timerAlien: Timer!
     var timerRocket: Timer!
+    var timerComet: Timer!
     
     var alienExploded = 0
     var controlRotation = 0
@@ -95,6 +98,17 @@ class MainViewController: UIViewController {
         rocket.isHidden = true
     }
     
+    func createComet()
+    {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0, execute: {
+            self.timerComet =  Timer.scheduledTimer(timeInterval: 0.002,
+                                               target: self,
+                                               selector: #selector(self.animationComet),
+                                               userInfo: nil,
+                                               repeats: true)
+        })
+    }
+    
     @objc func animationAlien()
     {
         alien.frame.origin.x = alien.frame.origin.x - 1
@@ -130,6 +144,13 @@ class MainViewController: UIViewController {
         {
             alienExploded += 1
             score.text = String(alienExploded)
+            explosion.center = alien.center
+            self.explosion.isHidden = false
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: {
+                self.explosion.isHidden = true
+            })
+            
             timerAlien.invalidate()
             timerRocket.invalidate()
             shot.isEnabled = true
@@ -141,12 +162,13 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad()
     {
-        rocketWidth = Int(rocket.frame.width)
-        rocketHeight = Int(rocket.frame.height)
+       // rocketWidth = Int(rocket.frame.width)
+       // rocketHeight = Int(rocket.frame.height)
         
         createAlien()
         createRocket()
-        
+        createComet()
+       
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -155,7 +177,27 @@ class MainViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @objc func animationComet()
+    {
+        let cos = __cospi(Double(-45)/Double.pi)
+        let sin = __sinpi(Double(-45)/Double.pi)
+        
+        comet.center.x -= CGFloat(cos)
+        comet.center.y -= CGFloat(sin)
+        
+        if comet.frame.origin.x < (rocket.frame.width * -1)
+        {
+            timerComet.invalidate()
+            comet.frame.origin.y = -100
+            
+            let width = UIScreen.main.bounds.size.width
+            let positionX =  Int(arc4random_uniform(UInt32(width - 100 + 1)) + 100)
+            
+            comet.frame.origin.x = CGFloat(positionX)
+            createComet()
+        }
+    }
 
 }
 
